de80f3a607705459ebb87de255b23a743afbc2e5
peppe-quint-boogaard <peppequintb@gmail.com>
2018-12-20 15:00:29 +0100

    changed data example

--- docs/script.js
+++ docs/script.js
@@ -3,6 +3,7 @@ const d3 = require("d3");
 
 // general values
 
+// https://codepen.io/znak/pen/qapRkQ
 const width = 100;
 const height = 100;
 
@@ -66,78 +67,6 @@ const data = {
           children: [
             { name: "Photo 1", value: "floorplan-1.jpg" },
             { name: "Photo 3", value: "floorplan-2.jpg" }
-          ]
-        }
-      ]
-    },
-    {
-      name: "Web",
-      children: [
-        {
-          name: "AEG",
-          children: [
-            { name: "Photo 1", value: "litho-1.jpg" },
-            { name: "Photo 2", value: "litho-2.jpg" },
-            { name: "Photo 3", value: "litho-3.jpg" },
-            { name: "Photo 4", value: "litho-4.jpg" },
-            { name: "Photo 5", value: "litho-5.jpg" },
-            { name: "Photo 6", value: "litho-6.jpg" }
-          ]
-        },
-        {
-          name: "Brent",
-          children: [
-            { name: "Photo 1", value: "digital-1.jpg" },
-            { name: "Photo 2", value: "digital-2.jpg" },
-            { name: "Photo 3", value: "digital-3.jpg" },
-            { name: "Photo 4", value: "digital-4.jpg" }
-          ]
-        }
-      ]
-    },
-    {
-      name: "Print",
-      children: [
-        {
-          name: "BvD",
-          children: [
-            { name: "Photo 1", value: "folding-1.jpg" },
-            { name: "Photo 2", value: "folding-2.jpg" },
-            { name: "Photo 3", value: "folding-3.jpg" }
-          ]
-        },
-        {
-          name: "Metalplast",
-          children: [
-            { name: "Photo 1", value: "stitched-1.jpg" },
-            { name: "Photo 2", value: "stitched-2.jpg" },
-            { name: "Photo 3", value: "stitched-3.jpg" },
-            { name: "Photo 4", value: "stitched-4.jpg" },
-            { name: "Photo 5", value: "stitched-5.jpg" }
-          ]
-        },
-        {
-          name: "Skylon",
-          children: [
-            { name: "Photo 1", value: "sewn-1.jpg" },
-            { name: "Photo 2", value: "sewn-2.jpg" },
-            { name: "Photo 3", value: "sewn-3.jpg" }
-          ]
-        },
-        {
-          name: "The O2",
-          children: [
-            { name: "Photo 1", value: "softback-1.jpg" },
-            { name: "Photo 3", value: "softback-2.jpg" }
-          ]
-        },
-        {
-          name: "Trendy",
-          children: [
-            { name: "Photo 1", value: "hardback-1.jpg" },
-            { name: "Photo 2", value: "hardback-2.jpg" },
-            { name: "Photo 3", value: "hardback-3.jpg" },
-            { name: "Photo 4", value: "hardback-4.jpg" }
           ]
         }
       ]
