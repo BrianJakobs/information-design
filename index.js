8c96749e241b5eace4eae2ed4297c7006a0450dd
peppe-quint-boogaard <peppequintb@gmail.com>
2019-01-14 13:36:29 +0100

    only scroll function, deleted unnecessary lines

--- index.js
+++ index.js
@@ -14,13 +14,6 @@ const y = d3
   .domain([0, height])
   .range([0, height]);
 
-const color = d3.scaleOrdinal().range(
-  d3.schemeDark2.map(function(c) {
-    c = d3.rgb(c);
-    return c;
-  })
-);
-
 const treemap = d3
   .treemap()
   .size([height, width])
@@ -33,33 +26,26 @@ const data = {
   children: [
     {
       name: "Non Digital Radio",
-      value: "non-digital-radio",
-      img: "non-digital-radio-image.png"
+      value: "non-digital-radio"
     },
     {
       name: "Digital Radio",
       value: "digital-radio",
-      img: "digital-radio-image.png",
+
       children: [
         {
           name: "PRO",
-          value: "pro-image",
-          img: "pro-image.png",
+          value: "pro",
+
           children: [
             {
               name: "Music Publisher",
               value: "music-publisher",
-              img: "music-publisher-image.png",
+
               children: [
                 {
                   name: "Song Writer",
-                  value: "song-writer",
-                  img: "song-writer-image.png"
-                },
-                {
-                  name: "Componist",
-                  value: "componist",
-                  img: "componist-image.png"
+                  value: "song-writer"
                 }
               ]
             }
@@ -68,12 +54,10 @@ const data = {
         {
           name: "Record Label",
           value: "record-label",
-          img: "record-label-image.png",
           children: [
             {
-              name: "Recording Artist",
-              value: "record-artist",
-              img: "recording-artist-image.png"
+              name: "Performer",
+              value: "performer"
             }
           ]
         }
@@ -82,16 +66,12 @@ const data = {
   ]
 };
 
-const nodes = d3
-  .hierarchy(data)
-  .count(function(d) {
-    return d.value ? 1 : 0;
-  })
-  .sort(function(a, b) {
-    return b.value - a.value;
-  });
-
-let currentDepth;
+const nodes = d3.hierarchy(data).count(function(d) {
+  return d.value ? 1 : 0;
+});
+// .sort(function(a, b) {
+//   return b.value - a.value;
+// });
 
 treemap(nodes);
 
@@ -103,7 +83,7 @@ const cells = chart
   .enter()
   .append("article")
   .attr("class", function(d) {
-    return "node level-" + d.depth;
+    return "node hide level-" + d.depth;
   })
   .attr("id", function(d) {
     return d.data.value;
@@ -113,80 +93,23 @@ const cells = chart
   });
 
 cells
-  .style("left", function(d) {
-    return x(d.x0) + "%";
-  })
-  .style("top", function(d) {
-    return y(d.y0) + "%";
-  })
-  .style("width", function(d) {
-    return d.x1 - d.x0 + "%";
-  })
-  .style("height", function(d) {
-    return y(d.y1) - y(d.y0) + "%";
-  })
-  // .style("background-image", function(d) {
-  //   return d.value ? "url(src/img/" + d.data.img + ")" : "none";
+  // .style("left", function(d) {
+  //   return x(d.x0) + "%";
   // })
-  .style("background-color", function(d) {
-    while (d.depth > 6) d = d.parent;
-    return color(d.data.name);
-  })
-  .on("click", zoom)
+  // .style("top", function(d) {
+  //   return y(d.y0) + "%";
+  // })
+  // .style("width", function(d) {
+  //   return d.x1 - d.x0 + "%";
+  // })
+  // .style("height", function(d) {
+  //   return d.y1 - d.y0 + "%";
+  // })
   .append("p")
   .attr("class", "label")
   .text(function(d) {
     return d.data.name ? d.data.name : "---";
   });
-
-const parent = d3
-  .select(".up")
-  .datum(nodes)
-  .on("click", zoom);
-
-// wanneer er geklikt wordt, gaat deze functie in werking
-function zoom(d) {
-  currentDepth = d.depth;
-  parent.datum(d.parent || nodes);
-
-  x.domain([d.x0, d.x1]);
-  y.domain([d.y0, d.y1]);
-
-  const transition = d3
-    .transition()
-    .duration(800)
-    .ease(d3.easeCubicOut);
-
-  cells
-    .transition(transition)
-    .style("left", function(d) {
-      return x(d.x0) + "%";
-    })
-    .style("top", function(d) {
-      return y(d.y0) + "%";
-    })
-    .style("width", function(d) {
-      return x(d.x1) - x(d.x0) + "%";
-    })
-    .style("height", function(d) {
-      return y(d.y1) - y(d.y0) + "%";
-    });
-
-  cells
-    .filter(function(d) {
-      return d.ancestors();
-    })
-    .classed("hide", function(d) {
-      return d.children ? true : false;
-    });
-
-  cells
-    .filter(function(d) {
-      return d.depth > currentDepth;
-    })
-    .classed("hide", false);
-  console.log(currentDepth);
-}
 
 // list of all json animations
 const aninmationSong = bodymovin.loadAnimation({
@@ -197,10 +120,9 @@ const aninmationSong = bodymovin.loadAnimation({
   path: "src/data/song_animation.json"
 });
 
-// functie(zoom) activeren op basis van coordinaten
-// check wheelDelta
+document.getElementsByClassName("level-0")[0].classList.remove("hide");
 
-// when user scrolls down the page, detect when sections hit the indidcator
+// when user scrolls down the page, detect when sections hit the measure point
 document.getElementsByTagName("body")[0].onscroll = () => {
   let measuringPoint = window.innerHeight / 2;
   let positionBody = document.body.getBoundingClientRect();
@@ -238,6 +160,18 @@ document.getElementsByTagName("body")[0].onscroll = () => {
     .getBoundingClientRect();
   const sectionSix = positionSectionSix.top - positionBody.top - measuringPoint;
 
+  const positionSectionSeven = document
+    .getElementById("section-heading-seven")
+    .getBoundingClientRect();
+  const sectionSeven =
+    positionSectionSeven.top - positionBody.top - measuringPoint;
+
+  const positionSectionEight = document
+    .getElementById("section-heading-eight")
+    .getBoundingClientRect();
+  const sectionEight =
+    positionSectionEight.top - positionBody.top - measuringPoint;
+
   if (window.pageYOffset < sectionTwo) {
     console.log("Song blijft staan tot...");
     document.getElementsByClassName("level-0")[0].classList.remove("hide");
@@ -255,12 +189,36 @@ document.getElementsByTagName("body")[0].onscroll = () => {
     // document.getElementsByClassName("level-0")[0].classList.add("hide");
     document.getElementById("non-digital-radio").classList.add("hide");
     document.getElementById("digital-radio").classList.add("hide");
+    document.getElementById("record-label").classList.remove("hide");
+    document.getElementById("pro").classList.remove("hide");
   } else if (
     window.pageYOffset > sectionFour &&
     window.pageYOffset < sectionFive
   ) {
     // document.getElementsByClassName("level-0")[0].classList.add("hide");
     document.getElementById("record-label").classList.add("hide");
-    document.getElementById("pro-image").classList.add("hide");
+    document.getElementById("pro").classList.add("hide");
+    document.getElementById("performer").classList.remove("hide");
+    // document.getElementById("").classList.remove("hide");
+  } else if (
+    window.pageYOffset > sectionFive &&
+    window.pageYOffset < sectionSix
+  ) {
+    document.getElementById("record-label").classList.remove("hide");
+    document.getElementById("pro").classList.remove("hide");
+    document.getElementById("performer").classList.add("hide");
+  } else if (
+    window.pageYOffset > sectionSix &&
+    window.pageYOffset < sectionSeven
+  ) {
+    document.getElementById("record-label").classList.add("hide");
+    document.getElementById("pro").classList.add("hide");
+    document.getElementById("music-publisher").classList.remove("hide");
+  } else if (
+    window.pageYOffset > sectionSeven &&
+    window.pageYOffset < sectionEight
+  ) {
+    document.getElementById("music-publisher").classList.add("hide");
+    document.getElementById("song-writer").classList.remove("hide");
   }
 };
